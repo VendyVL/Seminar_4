2615043bd2faaffe529853a32e0cb9078ee21de6
«VendyVL» <vedmansk@yandex.ru>
2022-05-24 09:39:36 +0300

    задача 29

---
+++ Ex_29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задаёт массив целых чисел и выводит их на экран. Размер массива определяется пользователем.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+Console.WriteLine("Ведите размер массива");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] array1 = new int[num];
+
+int[] PrintArray (int[]array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+        Console.Write(array[i]+" ");
+    }
+    return array;
+}
+
+PrintArray (array1);
